cd5adce07887c18c9d600cb10d3441b607c19ca3
Leonardo Vieira <leonardovieira_94@hotmail.com>
2024-07-23 08:39:16 -0300

    Criado Projeto baseado em aula C#

---
+++ Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using Test.Entities;
+using Test.Entities.Enums;
+using System.Globalization;
+
+namespace Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date(DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string prodName = Console.ReadLine();
+                Console.Write("Product Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(prodName, price);
+
+                OrderItem item = new OrderItem(quantity, price, product);
+
+                order.Items.Add(item);
+            }
+
+            Console.WriteLine(order);
+
+
+        }
+    }
+}
